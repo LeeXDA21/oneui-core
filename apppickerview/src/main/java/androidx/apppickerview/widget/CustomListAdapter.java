5ee2282a738438145bd02fe1323975a6b75796d2
LeeXDA21 <43325188+LeeXDA21@users.noreply.github.com>
2022-09-19 21:01:55 +0100

    Update CustomListAdapter.java

--- apppickerview/src/main/java/androidx/apppickerview/widget/CustomListAdapter.java
+++ apppickerview/src/main/java/androidx/apppickerview/widget/CustomListAdapter.java
@@ -9,50 +9,51 @@ import androidx.apppickerview.widget.AppPickerView;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
-/* loaded from: C:\Users\LeeXD\Documents\sesl5port\sesl5.dex */
+// Credits to Samsung, all rights reserved to the original owners.
+
 public class CustomListAdapter extends ListAdapter {
     public CustomListAdapter(Context context, int i, int i2, AppPickerIconLoader appPickerIconLoader) {
         super(context, i, i2, appPickerIconLoader);
         setHasStableIds(true);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter
-    public /* bridge */ /* synthetic */ AppPickerView.AppLabelInfo getAppInfo(int i) {
+    @Override
+    public AppPickerView.AppLabelInfo getAppInfo(int i) {
         return super.getAppInfo(i);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, android.widget.Filterable
-    public /* bridge */ /* synthetic */ Filter getFilter() {
+    @Override
+    public Filter getFilter() {
         return super.getFilter();
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, androidx.recyclerview.widget.RecyclerView.Adapter
-    public /* bridge */ /* synthetic */ int getItemCount() {
+    @Override
+    public int getItemCount() {
         return super.getItemCount();
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, androidx.recyclerview.widget.RecyclerView.Adapter
-    public /* bridge */ /* synthetic */ long getItemId(int i) {
+    @Override
+    public long getItemId(int i) {
         return super.getItemId(i);
     }
 
-    @Override // androidx.apppickerview.widget.ListAdapter, androidx.recyclerview.widget.RecyclerView.Adapter
-    public /* bridge */ /* synthetic */ int getItemViewType(int i) {
+    @Override
+    public int getItemViewType(int i) {
         return super.getItemViewType(i);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, android.widget.SectionIndexer
-    public /* bridge */ /* synthetic */ int getPositionForSection(int i) {
+    @Override
+    public int getPositionForSection(int i) {
         return super.getPositionForSection(i);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, android.widget.SectionIndexer
-    public /* bridge */ /* synthetic */ int getSectionForPosition(int i) {
+    @Override
+    public int getSectionForPosition(int i) {
         return super.getSectionForPosition(i);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, android.widget.SectionIndexer
-    public /* bridge */ /* synthetic */ Object[] getSections() {
+    @Override
+    public Object[] getSections() {
         return super.getSections();
     }
 
@@ -72,29 +73,28 @@ public class CustomListAdapter extends ListAdapter {
         resetPackages(list, false, list2, list3);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter, androidx.recyclerview.widget.RecyclerView.Adapter
-    public /* bridge */ /* synthetic */ void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
         super.onBindViewHolder(viewHolder, i);
     }
 
-    @Override // androidx.apppickerview.widget.ListAdapter, androidx.recyclerview.widget.RecyclerView.Adapter
-    /* renamed from: onCreateViewHolder */
-    public /* bridge */ /* synthetic */ RecyclerView.ViewHolder mo209onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return super.mo209onCreateViewHolder(viewGroup, i);
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return super.onCreateViewHolder(viewGroup, i);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter
-    public /* bridge */ /* synthetic */ void setOnBindListener(@NonNull AppPickerView.OnBindListener onBindListener) {
+    @Override
+    public void setOnBindListener(@NonNull AppPickerView.OnBindListener onBindListener) {
         super.setOnBindListener(onBindListener);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter
-    public /* bridge */ /* synthetic */ void setOnSearchFilterListener(AppPickerView.OnSearchFilterListener onSearchFilterListener) {
+    @Override
+    public void setOnSearchFilterListener(AppPickerView.OnSearchFilterListener onSearchFilterListener) {
         super.setOnSearchFilterListener(onSearchFilterListener);
     }
 
-    @Override // androidx.apppickerview.widget.AbsAdapter
-    public /* bridge */ /* synthetic */ void setOrder(int i) {
+    @Override
+    public void setOrder(int i) {
         super.setOrder(i);
     }
 }
